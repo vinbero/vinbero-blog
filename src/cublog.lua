57a9049992547226761c43f2a3e1902ed677cbb6
Byeonggon Lee <gonny952@gmail.com>
2016-09-06 14:18:44 +0900

    request.headers

--- src/cublog.lua
+++ src/cublog.lua
@@ -1,10 +1,10 @@
 math.randomseed(os.time())
 requestMapping = {}
 requestMapping['/'] = {}
-requestMapping['/']['GET'] = function(client)
+requestMapping['/']['GET'] = function(request)
     local body = function()
         coroutine.yield('<h1>Hello World!</h1>')
-        for key, value in pairs(client) do
+        for key, value in pairs(request) do
             if type(value) == 'string' then
                 coroutine.yield('<h2>' .. key .. ': ' .. value .. '</h2>')
             end
@@ -14,8 +14,8 @@ requestMapping['/']['GET'] = function(client)
 end
 
 requestMapping['/access-token'] = {}
-requestMapping['/access-token']['POST'] = function(client)
-    if client.parameter['id'] == 'admin' and client.parameter['password'] == 'password' then
+requestMapping['/access-token']['POST'] = function(request)
+    if request.parameter['id'] == 'admin' and request.parameter['password'] == 'password' then
         return 200, {['Content-Type'] = 'text/html; charset=utf8'}, math.random()
     else
         return 401, {}, '401 Unauthorized'
@@ -23,61 +23,61 @@ requestMapping['/access-token']['POST'] = function(client)
 end
 
 requestMapping['/error'] = {}
-requestMapping['/error']['GET'] = function(client)
+requestMapping['/error']['GET'] = function(request)
     return 500, {}, '500 Internal Server Error'
 end
 
 requestMapping['/empty'] = {}
-requestMapping['/empty']['GET'] = function(client)
+requestMapping['/empty']['GET'] = function(request)
     return 200, {}
 end
 
 requestMapping['/hello'] = {}
-requestMapping['/hello']['GET'] = function(client)
+requestMapping['/hello']['GET'] = function(request)
     return 200, {['Content-Type'] = 'text/html; charset=utf8'} , '<h1>Hello World</h1>'
 end
 
 requestMapping['/image'] = {}
-requestMapping['/image']['GET'] = function(client)
+requestMapping['/image']['GET'] = function(request)
     local file = assert(io.open('tux.jpg', 'r'))
     local body = file:read('*a')
     file:close()
     return 200, {['Content-Type'] = 'image/jpeg'}, body
 end
 
-function onRequestStart(client)
+function onRequestStart(request)
 end
 
-function onHeadersFinish(client)
+function onHeadersFinish(request)
 end
 
-function getContentLength(client)
-    return client['CONTENT_LENGTH']
+function getContentLength(request)
+    return request['CONTENT_LENGTH']
 end
 
-function onBodyChunk(client, body_chunk)
+function onBodyChunk(request, body_chunk)
     print('onBodyChunk ' .. body_chunk)
-    client['BODY'] = client['BODY'] .. body_chunk
+    request['BODY'] = request['BODY'] .. body_chunk
 end
 
-function onBodyFinish(client)
-    print(client['BODY'])
-    client['QUERY_STRING'] = client['BODY']
+function onBodyFinish(request)
+    print(request['BODY'])
+    request['QUERY_STRING'] = request['BODY']
 end
 
-function onRequestFinish(client)
+function onRequestFinish(request)
     print('onRequestFinish')
-    client.parameter = {}
-    if client['QUERY_STRING'] ~= nil then
-        for pair in string.gmatch(client['QUERY_STRING'], '([^&]+)') do
+    request.parameter = {}
+    if request['QUERY_STRING'] ~= nil then
+        for pair in string.gmatch(request['QUERY_STRING'], '([^&]+)') do
             for key, value in string.gmatch(pair, '([^=]+)=([^=]+)') do
-                client.parameter[key] = value
+                request.parameter[key] = value
             end
         end
     end
-    print(client['PATH_INFO'])
-    if requestMapping[client['PATH_INFO']] ~= nil and requestMapping[client['PATH_INFO']][client['REQUEST_METHOD']] ~= nil then
-        return requestMapping[client['PATH_INFO']][client['REQUEST_METHOD']](client)
+    print(request['PATH_INFO'])
+    if requestMapping[request['PATH_INFO']] ~= nil and requestMapping[request['PATH_INFO']][request['REQUEST_METHOD']] ~= nil then
+        return requestMapping[request['PATH_INFO']][request['REQUEST_METHOD']](request)
     end
     return 404, {['Content-Type'] = 'text/html; charset=utf8'}, '404 Not Found'
 end
