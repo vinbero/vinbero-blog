94a2ec621cddd690a3316ff054c22fd4900fc663
Byeonggon Lee <gonny952@gmail.com>
2016-09-18 03:05:36 +0900

    CORS, SQL dump

--- src/cublog.lua
+++ src/cublog.lua
@@ -4,35 +4,35 @@ local posts = require 'cublog.model.posts'.Posts.new()
 local json = require 'json'
 
 router:setCallback('^/posts$', 'POST', function(request)
-    return 200, {['Content-Type'] = 'application/json; charset=utf8'}, posts:create(request.parameters['title'], request.parameters['text'], request.parameters['private'])
+    return 200, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, posts:create(request.parameters['title'], request.parameters['text'], request.parameters['private'])
 end)
 
 router:setCallback('^/posts$', 'GET', function(request)
-    return 200, {['Content-Type'] = 'application/json; charset=utf8'}, json.encode(posts:getAll()) 
+    return 200, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, json.encode(posts:getAll()) 
 end)
 
 router:setCallback('^/posts/(?<id>\\d+)$', 'GET', function(request)
     local post = posts:get(request.parameters['id'])
     if post ~= nil then
-        return 200, {['Content-Type'] = 'application/json; charset=utf8'}, json.encode(posts:get(request.parameters['id']))
+        return 200, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, json.encode(posts:get(request.parameters['id']))
     else
-        return 400, {['Content-Type'] = 'application/json; charset=utf8'}, 'null'
+        return 400, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, 'null'
     end
 end)
 
 router:setCallback('^/posts/(?<id>\\d+)$', 'PUT', function(request)
     if posts:update(request.parameters['id'], request.parameters['title'], request.parameters['text'], request.parameters['private']) then
-        return 200, {['Content-Type'] = 'application/json; charset=utf8'}, 'true'
+        return 200, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, 'true'
     else
-        return 500, {['Content-Type'] = 'application/json; charset=utf8'}, 'false'
+        return 500, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, 'false'
     end
 end)
 
 router:setCallback('^/posts/(?<id>\\d+)$', 'DELETE', function(request)
     if posts:delete(request.parameters['id']) then
-        return 200, {['Content-Type'] = 'application/json; charset=utf8'}, 'true'
+        return 200, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, 'true'
     else
-        return 500, {['Content-Type'] = 'application/json; charset=utf8'}, 'false'
+        return 500, {['Content-Type'] = 'application/json; charset=utf8', ['Access-Control-Allow-Origin'] = '*'}, 'false'
     end
 end)
 
