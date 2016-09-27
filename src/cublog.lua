333d3a6d3bd7d0d14739e8dccf383f5ed1a70e47
Byeonggon Lee <gonny952@gmail.com>
2016-09-28 06:57:21 +0900

    getContentLength

--- src/cublog.lua
+++ src/cublog.lua
@@ -84,10 +84,6 @@ router:setCallback("^/tokens/?$", "POST", function(request)
     return 403, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
 end)
 
-function getContentLength(request)
-    return request.contentLength
-end
-
 function onBodyChunk(request, bodyChunk)
     request.body = request.body .. bodyChunk
 end
