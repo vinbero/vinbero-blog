dc38dc3810962431876c20e40bc5086e26c4b58e
Byeonggon Lee <gonny952@gmail.com>
2016-11-16 18:01:47 +0900

    Revert "Revert "Go back to cookieless""
    This reverts commit 38c3ece5284b4bba09c9fd38c8cbb2c1dc903344.

--- src/cublog.lua
+++ src/cublog.lua
@@ -1,6 +1,5 @@
 local json = require "rapidjson"
 local urlDecoder = require "gonapps.url.decoder"
-local Cookie = require "gonapps.cookie"
 local urlQueryParser = require "gonapps.url.query.parser"
 
 local Router = require "tucube.http.router"
@@ -14,8 +13,8 @@ local posts = Posts.new()
 local tokens = Tokens.new()
 
 router:setCallback("^/posts/?$", "POST", function(request)
-    local cookie = Cookie.new(request.headers["COOKIE"])
-    if cookie.data["CublogToken"] == nil or not tokens:isValid(cookie.data["CublogToken"]) then
+    local ok, token = pcall(string.match, request.headers["AUTHORIZATION"], "Bearer (.+)")
+    if not ok or token == nil or not tokens:isValid(token) then
         return 403, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
     end
     local ok, post = pcall(json.decode, request.body)
@@ -36,8 +35,8 @@ end)
 router:setCallback("^/posts/(?<id>\\d+)$", "GET", function(request)
     local post = posts:get({["id"] = request.parameters["id"]})
     if post ~= nil then
-        local cookie = Cookie.new(request.headers["COOKIE"])
-        if (post.private == true or post.private == 1) and (cookie.data["CublogToken"] == nil or not tokens:isValid(cookie.data["CublogToken"])) then
+        local ok, token = pcall(string.match, request.headers["AUTHORIZATION"], "Bearer (.+)")
+        if (post.private == true or post.private == 1) and (not ok or token == nil or not tokens:isValid(token)) then
             return 403, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
         end
         return 200, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, json.encode(post)
@@ -46,8 +45,8 @@ router:setCallback("^/posts/(?<id>\\d+)$", "GET", function(request)
 end)
 
 router:setCallback("^/posts/(?<id>\\d+)$", "PUT", function(request)
-    local cookie = Cookie.new(request.headers["COOKIE"])
-    if cookie.data["CublogToken"] == nil or not tokens:isValid(cookie.data["CublogToken"]) then
+    local ok, token = pcall(string.match, request.headers["AUTHORIZATION"], "Bearer (.+)")
+    if not ok or token == nil or not tokens:isValid(token) then
         return 403, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
     end
     local ok, post = pcall(json.decode, request.body)
@@ -61,8 +60,8 @@ router:setCallback("^/posts/(?<id>\\d+)$", "PUT", function(request)
 end)
 
 router:setCallback("^/posts/(?<id>\\d+)$", "DELETE", function(request)
-    local cookie = Cookie.new(request.headers["COOKIE"])
-    if cookie.data["CublogToken"] == nil or not tokens:isValid(cookie.data["CublogToken"]) then
+    local ok, token = pcall(string.match, request.headers["AUTHORIZATION"], "Bearer (.+)")
+    if not ok or token == nil or not tokens:isValid(token) then
         return 403, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
     end
     if posts:delete({["id"] = request.parameters["id"]}) then
@@ -77,10 +76,7 @@ router:setCallback("^/tokens/?$", "POST", function(request)
         return 400, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
     end
     if login.id == settings["ADMIN-ID"] and login.password == settings["ADMIN-PASSWORD"] then
-        local cookie = Cookie.new()
-        cookie.data["CublogToken"] = tokens:create()
-        cookie.flags["HttpOnly"] = true
-        return 200, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*", ["Set-Cookie"] = cookie:toString()}, "true"
+        return 200, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, json.encode(tokens:create())
     end 
     return 403, {["Content-Type"] = "application/json; charset=utf8", ["Access-Control-Allow-Origin"] = "*"}, "null"
 end)
